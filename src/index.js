0d863f4446104cf2bdb06f611e5139b87e554fe5
Tatevik Avetisyan <84620975+TatevAvetisyan-FrontEnd@users.noreply.github.com>
2022-04-21 15:46:12 +0400

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter} from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './Redux/store';
+
+import './index.css';
+import App from './App';
+
+
+
+ReactDOM.render(
+  <BrowserRouter>
+  <React.StrictMode>
+   <Provider store={store}>
+      <App />
+   </Provider> 
+  </React.StrictMode>
+  </BrowserRouter>,
+  document.getElementById('root')
+);
